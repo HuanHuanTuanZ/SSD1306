1fabb39b049be2cb2bab707e0cafcd02fa17260e
HuanHuanTuanZ <87598898+HuanHuanTuanZ@users.noreply.github.com>
2024-10-02 21:09:46 +0800

    Add files via upload

---
+++ auto.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#chkconfig:2345 22 80
+#description: Filebeat Service
+echo "OLED try running";
+
+date -s "2000-01-01 00:00:1"
+sudo /SSD1306/a.out &
---
+++ install.sh
@@ -0,0 +1,19 @@
+#! /bin/bash  # employ bash shell
+
+echo "Run Install"
+
+timedatectl set-timezone Asia/Shanghai
+apt-get install cron
+apt-get cmake
+apt-get install lm-sensors
+
+#DEV
+git clone https://github.com/wiringX/wiringX.git
+cd wiringX
+mkdir build
+cd build
+cmake ..
+make -j4
+cpack -G DEB
+sudo dpkg -i libwiringx*.deb
+
